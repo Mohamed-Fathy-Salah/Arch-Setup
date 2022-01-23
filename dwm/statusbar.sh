f117d5552df9e2017d2ebf071f005757d3d0f726
Mohamed Fathy <mofasa.peace@gmail.com>
2022-01-23 18:57:28 +0000

    status bar changes

--- dwm/statusbar.sh
+++ dwm/statusbar.sh
@@ -1,7 +1,23 @@
-#!/bin/sh
+#!/bin/zsh
+
+get_date() {
+    date +'%a | %b %d | %r'
+}
+
+get_battery() {
+     cat /sys/class/power_supply/BAT0/capacity
+}
+
+get_netspeed(){}
+
+get_language(){
+    setxkbmap -query | awk '/layout/ {print $2}'
+}
+
+get_sound(){}
+
 while true
 do
-    xsetroot -name "$(date +'%a | %b %d | %r')"
+    xsetroot -name "$(get_date)    [$(get_language)]    $(get_battery)%"
     sleep 1
 done
-
