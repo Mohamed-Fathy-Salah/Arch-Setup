3a0ba01e10849d6cd1c4e01f8134d6f41439b13b
Mohamed Fathy <mofasa.peace@gmail.com>
2022-01-26 08:35:27 +0000

    net speed added to status bar

--- dwm/statusbar.sh
+++ dwm/statusbar.sh
@@ -1,20 +1,5 @@
 #!/bin/zsh
 
-get_date() {
-    date="$(LC_ALL=C date "+%a %d %b")"
-    echo -ne "  ${date} "
-}
-
-get_time() {
-    time="$(date "+%I:%M %p")"
-    echo -ne "  ${time} "
-}
-
-get_wifi() {
-    wifiperc="$(grep "^\s*w" /proc/net/wireless | awk '{ print int($3 * 100 / 70) "%" }')"
-    echo -ne "  ${wifiperc}"
-}
-
 get_language(){
     case "$(xset -q|grep LED| awk '{ print $10 }')" in
         "00000000") echo -ne "en" ;;
@@ -35,13 +20,18 @@ get_volume() {
     fi
 }
 
+
 while true
 do
-    BAT=$(cat /sys/class/power_supply/BAT0/capacity)
+    DATE="  $(date "+%a %d %b") "
+    TIME="  $(date "+%I:%M %p") "
     MEM=$(free -h --kilo | awk '/^Mem:/ {print $3 "/" $2}')
     CPU=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}' )
     TEMP=$(sensors|grep 'Core 0'|awk '{print $3}' )
+    NET=$( awk '/wlan0/ {i++; rx[i]=$2; tx[i]=$10}; END{printf "▼ %.1f KB - ▲ %.1f KB" ,(rx[2]-rx[1])/1024 , (tx[2]-tx[1])/1024 }' <(cat /proc/net/dev; sleep 1; cat /proc/net/dev))
+    BAT=$(cat /sys/class/power_supply/BAT0/capacity)
 
-    xsetroot -name "$(get_date)  |  $(get_time)  |  $(get_volume)  |     $MEM  |    $CPU%  |    $TEMP  |  $(get_language)  |  $(get_wifi)  |  $BAT% "
+    xsetroot -name "$DATE  |  $TIME  |  $(get_volume)  |   $MEM  |   $CPU%  |   $TEMP  |  $(get_language)  |  $NET  |  $BAT% "
+
     sleep 2
-done
+done &
