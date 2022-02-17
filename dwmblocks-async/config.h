0d64a9fc75f0dea46128a7e1a7cd9448366943f2
Mohamed Fathy <mofasa.peace@gmail.com>
2022-02-17 16:13:49 +0200

    dwm blocks

---
+++ dwmblocks-async/config.h
@@ -0,0 +1,15 @@
+#define CMDLENGTH 100
+#define DELIMITER " ┃ "
+//#define CLICKABLE_BLOCKS
+
+const Block blocks[] = {
+	BLOCK("sb-date",    0,    1),
+	BLOCK("sb-time",    60,   2),
+	BLOCK("sb-memory",  5,    3),
+	BLOCK("sb-loadavg", 5,    4),
+	BLOCK("sb-temp",    5,    5),
+	BLOCK("sb-volume",  0,    6),
+	BLOCK("sb-mic",     0,    7),
+	BLOCK("sb-network", 5,    9),
+	BLOCK("sb-lang",    0,    8),
+};
