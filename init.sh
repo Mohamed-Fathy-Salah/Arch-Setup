4238743f9b4b4e9e0863a47ba8621cb59d1e71b0
Mohamed Fathy <mofasa.peace@gmail.com>
2022-03-09 09:28:48 +0200

    modified:   init.sh

--- init.sh
+++ init.sh
@@ -5,8 +5,14 @@ chsh -s $(which zsh)
 # install xcompmgr if u want opacity in terminal
 sudo pacman -Syu base-devel libX11-devel libXft-devel libXinerama-devel freetype-devel fontconfig-devel gvim xorg lf sxiv dmenu htop maim w3m python3 ueberzug ffmpeg chafa exiftool ImageMagick xclip xdotool pulseaudio vscode pamixer ncmpcpp calcurse pcmanfm gvfs kdeconnect git curl wget xournalpp mpv xwallpaper gromit-mpxpoppler libreoffice
 
-cd ~/apps/suckless/dwm ; sudo make clean install
-cd ~/apps/suckless/st  ; sudo make clean install
-cd ~/apps/suckless/dwmblocks-async  ; sudo make clean install
-cd ~/apps/suckless/dmenu ; sudo make clean install
-cd ~/apps/suckless/stpv ; sudo make install
+mkdir ~/apps ; cd ~/apps
+git clone git@github.com:Mohamed-Fathy-Salah/Arch-Setup.git
+
+cd ~/apps/Arch-Setup/dwm ; sudo make clean install
+cd ~/apps/Arch-Setup/st  ; sudo make clean install
+cd ~/apps/Arch-Setup/dwmblocks-async  ; sudo make clean install
+cd ~/apps/Arch-Setup/dmenu ; sudo make clean install
+
+cd ~/apps
+git clone https://github.com/Naheel-Azawy/stpv.git
+cd stpv ; sudo make install
