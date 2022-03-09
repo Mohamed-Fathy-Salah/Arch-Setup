515e6c52c94e925c399d9fdb3a24b9868479bc63
Mohamed Fathy <60598620+Mohamed-Fathy-Salah@users.noreply.github.com>
2022-03-09 17:06:15 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,14 +5,15 @@ chsh -s $(which zsh)
 # install xcompmgr if u want opacity in terminal
 sudo pacman -Syu base-devel libX11-devel libXft-devel libXinerama-devel freetype-devel fontconfig-devel gvim xorg lf sxiv dmenu htop maim w3m python3 ueberzug ffmpeg chafa exiftool ImageMagick xclip xdotool pulseaudio vscode pamixer ncmpcpp calcurse pcmanfm gvfs kdeconnect git curl wget xournalpp mpv xwallpaper gromit-mpxpoppler libreoffice
 
-mkdir ~/apps ; cd ~/apps
-git clone git@github.com:Mohamed-Fathy-Salah/Arch-Setup.git
+#mkdir ~/apps ; cd ~/apps
+#git clone git@github.com:Mohamed-Fathy-Salah/Arch-Setup.git
 
 cd ~/apps/Arch-Setup/dwm ; sudo make clean install
 cd ~/apps/Arch-Setup/st  ; sudo make clean install
 cd ~/apps/Arch-Setup/dwmblocks-async  ; sudo make clean install
 cd ~/apps/Arch-Setup/dmenu ; sudo make clean install
 
-cd ~/apps
-git clone https://github.com/Naheel-Azawy/stpv.git
-cd stpv ; sudo make install
+#cd ~/apps
+#git clone https://github.com/Naheel-Azawy/stpv.git
+#cd stpv ; sudo make install
+cd ~/apps/stpv ; sudo make install
